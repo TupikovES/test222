1ff4354ca5b708e63f51cd04a20f289ff43e1b4e
Tupikov_ES <TupikovES@samnipineft.ru>
2018-03-22 15:57:26 +0400

    add method to second branch

--- src/com/company/module/ModuleClass.java
+++ src/com/company/module/ModuleClass.java
@@ -4,4 +4,17 @@ package com.company.module;
  * New module
  */
 public class ModuleClass {
+
+    private int m;
+
+    public ModuleClass() {
+    }
+
+    public int getM() {
+        return m;
+    }
+
+    public void setM(int m) {
+        this.m = m;
+    }
 }
